27c9030278b6648fe54e3039e0fad55561b02dc4
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-03-28 02:47:28 +0530

    feat(app): integrate components, cookie checks

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,19 @@ import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import { HttpExceptionTransformer } from 'http-exception-transformer'
 import { initializeConnection } from './service/database/cassandra'
+import { cookieDecoder } from './service/cookie/decoder'
+
+import { roleStatusCheck } from './service/roles/definitions'
 
 /** link all modules onto application */
 import UserRoutes from './modules/user/routes'
-// import AuthRoutes from './modules/auth/routes'
+import AuthRoutes from './modules/auth/routes'
+import NoteRoutes from './modules/notes/routes'
+import { ensureLoggedIn } from './service/middleware/loggedIn.middleware'
 
 /** initialize database connections */
 initializeConnection()
+roleStatusCheck()
 
 /**
  * Initialize express application to hook all middleware
@@ -23,13 +29,17 @@ app.use(
 )
 app.use(bodyParser.json())
 app.use(cookieParser())
+app.use(cookieDecoder())
 
 app.get('', (req, res) => {
   res.json({ alive: true })
 })
 
 app.use('/user', UserRoutes)
-// app.use('/auth', AuthRoutes)
+app.use('/auth', AuthRoutes)
+
+app.use('/note', ensureLoggedIn())
+app.use('/note', NoteRoutes)
 
 app.use(HttpExceptionTransformer)
 export default app
